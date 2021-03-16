6de816f5092811b9c8cfa1dd2068a83e8dfedca1
Michał Darowny <michal.darowny@gmail.com>
2021-03-16 12:14:20 +0100

    initial

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import numpy as np
+conditions_num = 3
+brakes_num = 3
+choices_matrix = np.zeros((brakes_num, conditions_num))
+choices_matrix[0][:] = [85.00, 75.00, 95.00]
+choices_matrix[1][:] = [85.00, 90.00, 75.5]
+choices_matrix[2][:] = [85.00, 65.00, 92.00]
+print(choices_matrix)
